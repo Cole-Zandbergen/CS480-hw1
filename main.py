1294ce617c2277be0a50d1dea67f4a4f7b2c39fa
Cole-Zandbergen <Zandbergen.Cole@gmail.com>
2021-10-14 16:26:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,10 @@
 ####################################################
 
 filepath = input("Enter the filepath for the folder containing your CSV files: ")
+#get the user to enter the filepath
 
+ActivityChanges = open(filepath + '/activityChanges.csv', 'r')
+ACC = open(filepath + '/sensoringData_acc.csv')
+GPS = open(filepath + '/sensoringData_gps.csv')
+Gyro = open(filepath + '/sensoringData_gyro.csv')
+Magnetic = open(filepath + '/sensoringData_magn.csv')
